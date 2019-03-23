fbf1e1f5c95ea484ecf7c93a00067ff8bf78fe50
danielorozco14 <00200617@uca.edu.sv>
2019-03-23 10:55:50 -0600

    Agregada funcionalidad de compartir los datos.Terminado

--- app/src/main/java/com/example/ejerciciolabo3/utilities/AppConstants.java
+++ app/src/main/java/com/example/ejerciciolabo3/utilities/AppConstants.java
@@ -1,6 +1,5 @@
 package com.example.ejerciciolabo3.utilities;
 
 public class AppConstants {
-    public static final String TEXT_KEY="textKey";
     public static final String NAME_KEY="nameKey",PASS_KEY="passKey",EMAIL_KEY="emailKey",GENDER_KEY="genderKey";
 }
